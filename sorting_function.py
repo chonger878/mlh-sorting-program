9cf169bad4aec394bbd838c579da4a9aa4222c93
chonger878 <echong06@gmail.com>
2023-01-12 01:18:12 -0800

    first commit

---
+++ sorting_function.py
@@ -0,0 +1,7 @@
+bookList = ["The Hobbit", "All Quiet on the Western Front", 
+                "The Lion, The Witch, and the Wardrobe", "Hamlet",
+                "Beowolf", "Juliet and Romeo"]
+
+bookList.sort()
+print(bookList)
+    
